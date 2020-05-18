36bf5651f796c4aa7d84597fd032a9e4cb079555
weizheng <weizheng>
2020-05-18 21:11:52 +0800

    init

---
+++ src/main/java/wz/ling1991/FeignFormDemoApplication.java
@@ -0,0 +1,13 @@
+package wz.ling1991;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignFormDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignFormDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wz/ling1991/FeignFormDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package wz.ling1991;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignFormDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
